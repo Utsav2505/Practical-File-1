7e2525ab9ec881f2019ddcead5e7907819bdc669
utsav <utsav2005maji@gmail.com>
2023-11-25 18:11:02 +0530

    q3 done

---
+++ q3.c
@@ -0,0 +1,14 @@
+//Q3-Program to tell if input no. is positive/negative
+//By Utsav(AIDS-B1)
+#include <stdio.h> //Importing header files, in this case its stdio.h
+int main(){
+    int a; //declaring a integer variable to store the input integers.
+    printf("Enter a no. "); //printing a label to receive an integer.
+    scanf("%d",&a); //taking input from user for an integer
+
+    // Conditional/Ternary operator - is 'a' greater than or equal to 0 '?'
+    //, If yes then print "Entered no. is POSITIVE" otherwise ':' print "Entered no. is NEGATIVE";
+    (a>=0) ? printf("Entered no. is POSITIVE") : printf("Entered no. is NEGATIVE");
+
+    return 0; //Returning 0 from main funcn denoting program is finished.
+}
