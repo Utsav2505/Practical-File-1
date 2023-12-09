56fb23926289234dfad24efe8b18e0e3d8a5a5ff
utsav <utsav2005maji@gmail.com>
2023-12-09 12:29:40 +0530

    will now start with 7th

--- q3.c
+++ q3.c
@@ -7,7 +7,8 @@ int main(){
     scanf("%d",&a); //taking input from user for an integer
 
     // Conditional/Ternary operator - is 'a' greater than or equal to 0 '?'
-    //, If yes then print "Entered no. is POSITIVE" otherwise ':' print "Entered no. is NEGATIVE";
+    //, If yes then print "Entered no. is POSITIVE" 
+    //otherwise ':' print "Entered no. is NEGATIVE";
     (a>=0) ? printf("Entered no. is POSITIVE") : printf("Entered no. is NEGATIVE");
 
     return 0; //Returning 0 from main funcn denoting program is finished.
