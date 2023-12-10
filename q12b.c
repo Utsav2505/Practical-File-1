c0a159b9fd7b4ce0cb2d4df5c660e6a402a75c1e
utsav <utsav2005maji@gmail.com>
2023-12-10 14:15:41 +0530

    q12c

--- q12b.c
+++ q12b.c
@@ -1,4 +1,4 @@
-//Q12 (a)- Program to take n values in array and print in reverse order
+//Q12 (b)- Program to take n values in array and print the sum of them
 //by - Utsav(AIDS-B1)
 
 //importing header files, in this case its stdio.h
@@ -17,17 +17,14 @@ void input(int n,int arr[]){
         //increment value of i after end of code block
     }
 }
-//Creating a function to print array in reverse
-void print(int n, int arr[]){
-    //print label to print output
-    printf("Values of array in reverse order: ");
-    //init a loop starting from end index of array(n-1) 
-    //and run until it becomes zero
-    for(int i=n-1;i>=0;i--){
-        //print the value at each index
-        printf("%d , ",arr[i]);
-        //decrement the value of i after end of code block
+int sum(int n,int arr[]){
+    int ans=0;//declaring a variable to store sum
+    //init a loop starting from 0 and run till n-1
+    for(int i=0;i<n;i++){
+        ans += arr[i]; //adding values at aech array index
+        //increment value of  i
     }
+    return ans;//return the sum
 }
 int main(){
     int n;//declaring a variable to store input
@@ -39,7 +36,7 @@ int main(){
     //calling input function and print function
     //after passing appropriate parameters
     input(n,arr);
-    print(n,arr);
+    printf("The Sum of input nums is: %d",sum(n,arr));
     //return 0 after program is over
     return 0;
 
--- q12c.c
+++ q12c.c
@@ -1,4 +1,4 @@
-//Q12 (a)- Program to take n values in array and print in reverse order
+//Q12 (c)- Program to take n values in array and print the sum of them
 //by - Utsav(AIDS-B1)
 
 //importing header files, in this case its stdio.h
@@ -17,30 +17,39 @@ void input(int n,int arr[]){
         //increment value of i after end of code block
     }
 }
-//Creating a function to print array in reverse
-void print(int n, int arr[]){
+void copy(int n, int arr[],int arr2[]){
+    //init a loop starting from 0 and run till i=n-1
+    for(int i=0;i<n;i++){
+        //assigning values of arr2 to arr at each index
+        arr2[i]=arr[i];
+    }
+}
+void print(int n, int arr2[]){
     //print label to print output
-    printf("Values of array in reverse order: ");
+    printf("Values in Array2 are: ");
     //init a loop starting from end index of array(n-1) 
     //and run until it becomes zero
     for(int i=n-1;i>=0;i--){
         //print the value at each index
-        printf("%d , ",arr[i]);
+        printf("%d , ",arr2[i]);
         //decrement the value of i after end of code block
     }
 }
 int main(){
     int n;//declaring a variable to store input
     //printing label and taking input
-    printf("Enter the size of array:");
+    printf("Enter the size of Array1:");
     scanf("%d",&n);
     //declaring the array
     int arr[n];
-    //calling input function and print function
+    //calling input function, copy and print function
     //after passing appropriate parameters
     input(n,arr);
-    print(n,arr);
-    //return 0 after program is over
+    int arr2[n];
+    copy(n,arr,arr2);
+    //printing label 
+    printf("Array1 to copied to Array2 \n");
+    print(n,arr2);
+    //returning 0 after program ends
     return 0;
-
 }
